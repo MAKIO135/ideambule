4fa265135d76d559048ed0803a60c13dae8145b8
MAKIO135 <makio135@gmail.com>
2018-01-16 18:09:28 +0100

    remove comment

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-addEventListener( 'load', e => {
-	// socket connection + geoloc
-	const connectSocket = () => {
-		const socket = io();
-
-		socket.on( 'connected', data => {
-			console.log( data );
-		} );
-
-		socket.on( 'content', data => {
-			console.log( data );
-		} );
-
-		const getGeoloc = () => {
-			let options = {
-				enableHighAccuracy: true,
-				timeout: 5000,
-				maximumAge: 0
-			};
-
-			function success( pos ){
-				let coords = pos.coords;
-
-				console.log( 'Your current position is:' );
-				console.log( `Latitude : ${ coords.latitude }` );
-				console.log( `Longitude: ${ coords.longitude }` );
-				console.log( `More or less ${ coords.accuracy } meters.` );
-				socket.emit( 'located', { lat: coords.latitude, long: coords.longitude } );
-			};
-
-			function error( err ){
-				console.warn( `ERROR(${ err.code }): ${ err.message }` );
-			};
-
-			navigator.geolocation.getCurrentPosition( success, error, options );
-=======
-// const card = {
-// 	lastTouch: 0,
-// 	display: false,
-// 	x: -1,
-// 	y: -1,
-// 	w: 0,
-// 	h: 0
-// };
-
 const connectSocket = () => {
 	const socket = io();
 
@@ -70,7 +24,6 @@ const connectSocket = () => {
 			console.log( `Longitude: ${ coords.longitude }` );
 			console.log( `More or less ${ coords.accuracy } meters.` );
 			socket.emit( 'located', { lat: coords.latitude, long: coords.longitude } );
->>>>>>> f6d9a6ed3f016674cf351779693f8a8bbe467427
 		};
 		getGeoloc();
 	};
