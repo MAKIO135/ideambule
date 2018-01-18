5642630d84928f0aa4ad32a2e6dd1b473360a01a
MAKIO135 <makio135@gmail.com>
2018-01-18 14:43:02 +0100

    update proto

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
 const SerialPort = require( 'serialport' );
-// const serialPort = new SerialPort( 'COM8', { baudRate : 19200 } ); // windows
-const serialPort = new SerialPort( '/dev/ttyACM0', { baudRate : 19200 } ); // raspberry pi
+// const printerPort = new SerialPort( 'COM8', { baudRate : 19200 } ); // windows
+const arduinoPort = new SerialPort( '/dev/ttyACM0', { baudRate : 9600 } ); // raspberry pi
+const printerPort = new SerialPort( '/dev/ttyS0', { baudRate : 19200 } ); // raspberry pi
 const Printer = require( 'thermalprinter' );
 
-let path = __dirname + '/images/plan.png';
+let baseImagesPath = __dirname + '/images/';
+let startImagePath = baseImagesPath + 'start.jpg';
+let endImagePath = baseImagesPath + 'end.jpg';
 
-serialPort.on( 'open', () => {
-	let printer = new Printer( serialPort );
+let printer;
+let printerReady = false;
+printerPort.on( 'open', () => {
+	let opts = {
+		maxPrintingDots: 10,
+		heatingTime: 200,
+		heatingInterval: 2,
+		commandDelay: 3
+	};
+
+	printer = new Printer( printerPort, opts );
+
 	printer.on( 'ready', () => {
+		printerReady = true;
+		print( 1 );
+	} );
+} );
+
+arduinoPort.on( 'open' () => {
+	
+} );
+
+function print( n ){
+	if( printerReady ){
+		printerReady = false;
+
+		let cat = n == 0 ? 'm5' :
+				  n == 1 ? 'm15' :
+				  'p15';
+
+		// let imagePath = baseImagesPath + cat + '-' + ~~( Math.random() * 3 ) + '.jpg';
+		let imagePath = baseImagesPath + cat + '-' + ~~( Math.random() * 0 ) + '.jpg';
+
 		printer
-			// .indent( 10 )
-			// .horizontalLine( 16 )
-			// .bold( true )
-			// .indent( 10 )
-			// .printLine( 'first line' )
-			// .bold( false )
-			// .inverse( true )
-			// .big( true )
-			// .right()
-			// .printLine( 'second line')
-			.printImage( path)
+			.printImage( startImagePath )
+			.printImage( imagePath )
+			.printImage( endImagePath )
 			.print( () => {
+				printerReady = true;
 				console.log( 'done' );
 				process.exit();
 			} );
-	} );
-} );
+	}
+	else{
+		console.log( 'Waiting for printer' );
+	}
+}
